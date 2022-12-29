46f59426a770c28ba50f29d58a3110d27e8200ea
JayJHFE <dove92lee@gmail.com>
2022-12-29 20:30:45 +0900

    221229/add/카페리스트 페이지 퍼블리싱

---
+++ cafemoa/src/components/units/slider/Slider.Styles.ts
@@ -0,0 +1,66 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+
+
+export const CafeImage = styled.div`
+  width: 100%;
+  transition : all 0.5s;
+`
+export const CafeContent = styled.div`
+  width: 100%;
+  opacity: 0;
+  display: inline-block;
+  margin-left: 50px;
+//   background: red;
+  transform: translateX(-45px)translateY(100px); 
+  transition : all 0.8s;  
+`
+export const SlideWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  ${(props) => props.isActive && css`
+    ${CafeImage}{
+        transform: scale(1.2)
+    }
+    ${CafeContent}{
+        opacity: 1;
+        transform: translateX(-45px) translateY(40px)
+    }
+  `}
+`
+
+export const StyledSlider = styled(Slider)`
+  .slick-list {
+    width: 100%;
+    margin: 0 auto;
+    overflow: visible;
+  }
+  .slick-prev:before,
+  .slick-next:before {
+    color: black;
+    opacity: 1;
+    font-size: 30px;
+  }
+  .slick-prev {
+    left: 0px;
+    z-index: 9;
+  }
+  .slick-next {
+    right: 0px;
+    z-index: 9;
+  }
+  .slick-slide{
+    padding: 0 40px
+  }
+  .slick-slide img {
+    margin: 0 auto;
+  }
+  .slick-dots {
+    position: relative;
+    bottom: -380px;
+  }
+`;
---
+++ cafemoa/src/components/units/slider/Slider.index.tsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import Text from "../../common/text/01/Text01.index";
+import Tag from "../../common/text/02/Text02.index";
+import * as S from "./Slider.Styles"
+
+export default function SlickSlider() {
+    const [activeIdx , setActiveIdx] = useState(0);
+    const settings = {
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    };
+    return (
+      <div>
+        <S.StyledSlider {...settings} afterChange={(idx) => setActiveIdx(idx)}>
+          <S.SlideWrapper isActive={activeIdx === 5}>
+            {/* {data.fetchCafe.images.map((el,idx) => (
+                <S.SlideWrapper isActive={activeIdx === idx}>
+                <S.CafeImage><img src= {el} /></S.CafeImage>
+                <S.CafeContent>샬라샬라샬라</S.CafeContent>
+            </S.SlideWrapper>
+            ))} */}
+            <S.CafeImage><img src="/images/cafelist/Cafe1.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">코캠다방</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+          <S.SlideWrapper isActive={activeIdx === 0}>
+            <S.CafeImage><img src="/images/cafelist/Cafe2.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">스타벅스</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+          <S.SlideWrapper isActive={activeIdx === 1}>
+            <S.CafeImage><img src="/images/cafelist/Cafe3.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">투썸플레이스</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+          <S.SlideWrapper isActive={activeIdx === 2}>
+            <S.CafeImage><img src="/images/cafelist/Cafe1.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">커피빈</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+          <S.SlideWrapper isActive={activeIdx === 3}>
+            <S.CafeImage><img src="/images/cafelist/Cafe1.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">빽다방</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+          <S.SlideWrapper isActive={activeIdx === 4}>
+            <S.CafeImage><img src="/images/cafelist/Cafe1.jpeg"/></S.CafeImage>
+            <S.CafeContent>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+                <span style={{marginLeft: 40}}>
+                    <Tag size="md">태그</Tag>
+                </span>
+            </S.CafeContent>
+            <S.CafeContent>
+                <div style={{marginTop:30, marginLeft: 130}}>
+                    <Text size="32" weight="500">메가커피</Text>
+                </div>
+            </S.CafeContent>
+          </S.SlideWrapper>
+        </S.StyledSlider>
+      </div>
+    );
+  }
