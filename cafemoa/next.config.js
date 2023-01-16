1af29e8966448f1a8306a8f9f4e9fc8c1fd10bed
JayJHFE <dove92lee@gmail.com>
2023-01-16 22:02:46 +0900

    230116/modify/배포용 파일수정

--- cafemoa/next.config.js
+++ cafemoa/next.config.js
@@ -3,9 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  generateBuildId: () => "cafemoa_deploy",
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/cafe": { page: "/cafe" },
+    "/login": { page: "/login" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
